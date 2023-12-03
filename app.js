49da8666e0c7102741c33034cd5593c5061e8b09
vsmm-world <thekingjarvis792@gmail.com>
2023-12-03 10:59:05 +0530

    starting of Front-end

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
 const express = require('express');
 const app = express();
 const port = process.env.port || 5000;
+
+app.set('view engine', 'ejs');
+
+
+
+app.listen(port,()=>{
+    console.log(`website is runnig at : http://localhost:${port}`);
+
+})
+
+
+app.get('/',(req,res)=>{
+
+    res.render('home');
+})
