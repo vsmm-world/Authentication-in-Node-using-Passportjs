2b4528840108969261851c30f5a507d4a025ff26
vsmm-world <thekingjarvis792@gmail.com>
2023-12-03 11:35:30 +0530

    Database Connections

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const ConnectDB = require('./db/connection');
 const app = express();
 const port = process.env.port || 5000;
 
+
 app.set('view engine', 'ejs');
 
-
+ConnectDB()
 
 app.listen(port,()=>{
     console.log(`website is runnig at : http://localhost:${port}`);
