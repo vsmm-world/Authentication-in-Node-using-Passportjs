7d3f803ec19daadd2d2db2b4a8a2c8f0d5b566b8
vsmm-world <thekingjarvis792@gmail.com>
2023-12-08 22:19:00 +0530

    updating front-end

--- routes/route.js
+++ routes/route.js
@@ -15,18 +15,4 @@ router.route('/login').post(passport.authenticate('local'), (req, res) => {
 
 router.route('/img').
 
-
-// const upload = multer({ storage: storage })
-// const cpUpload = upload.fields([{ name: 'avatar', maxCount: 1 }, { name: 'gallery', maxCount: 8 }])
-
-// app.post('/profile', cpUpload, function (req, res, next) {
-//     // req.files is an object (String -> Array) where fieldname is the key, and the value is array of files
-//     //
-//     // e.g.
-//     //  req.files['avatar'][0] -> File
-//     //  req.files['gallery'] -> Array
-//     //
-//     // req.body will contain the text fields, if there were any
-//   })
-
 module.exports = router;
